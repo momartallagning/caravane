3ad2117ef6dee86f03efc5e08981914fb598f149
momartallagning <63287312+momartallagning@users.noreply.github.com>
2020-06-13 14:55:09 +0000

    Add files via upload

---
+++ mdl.py
@@ -0,0 +1,29 @@
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        if not is_superuser:
+            disabled_fields |= {
+                'email',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
